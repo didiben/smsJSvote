159cd81609fa6cb73f4bbd57a673505d0b9c822d
Benoît Delamea <Benoit@Benoits-MacBook-Pro.local>
2018-03-26 15:31:43 +0200

    two-methods app - UNTESTED

--- platforms/android/app/build/intermediates/assets/debug/www/js/app.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/app.js
@@ -23,30 +23,126 @@ angular.module('starter', ['ionic', 'ngCordova'])
     });
 })
 .controller('SMSlister', ['$scope', '$ionicPlatform', function($scope, $ionicPlatform){
+    $scope.running = false;
+    $scope.smsRecieved = new Array();
+    $scope.results1 = 0;
+    $scope.results2 = 0;
+    $scope.results3 = 0;
+    $scope.results4 = 0;
+    $scope.results5 = 0;
+    $scope.results6 = 0;
+    $scope.results7 = 0;
     $scope.SMSlist = function() {
         $ionicPlatform.ready(function() {
-            $scope.log1 = '--> SMSlist launched';
+            console.log('############# verifyNumbers launched ! #############');
+            $scope.running = true;
             var filter = {
-                box : 'inbox', // 'inbox' (default), 'sent', 'draft'
+                box : 'inbox', // 'inbox' (default), 'sent', 'draft', 'outbox', 'failed', 'queued', and '' for all
+                read : 0, // 0 for unread SMS, 1 for SMS already read
                 indexFrom : 0, // start from index 0
-                maxCount : 10, // count of SMS to return each time
+                maxCount : 500 // count of SMS to return each time
             };
+            // if(SMS) SMS.startWatch(function(){
+            //     document.addEventListener('onSMSArrive', function(e){
+            //         var sms = e.data;
+            //         smsList.push( sms );
+            //         switch (sms.body) {
+            //             case '1':
+            //             $scope.results1 = $scope.results1 + 1;
+            //             deletenewarrivant(sms.address);
+            //             break;
+            //             case '2':
+            //             $scope.results2 = $scope.results2 + 1;
+            //             deletenewarrivant(sms.address);
+            //             break;
+            //             case '3':
+            //             $scope.results3 = $scope.results3 + 1;
+            //             deletenewarrivant(sms.address);
+            //             break;
+            //             case '4':
+            //             $scope.results4 = $scope.results4 + 1;
+            //             deletenewarrivant(sms.address);
+            //             break;
+            //             case '5':
+            //             $scope.results5 = $scope.results5 + 1;
+            //             deletenewarrivant(sms.address);
+            //             break;
+            //             case '6':
+            //             $scope.results6 = $scope.results6 + 1;
+            //             deletenewarrivant(sms.address);
+            //             break;
+            //             default:
+            //             $scope.results7 = $scope.results7 + 1;
+            //             deletenewarrivant(sms.address);
+            //         }
+            //     });
+            // }, function(){
+            //     console.log('Error !!');
+            //     $scope.errDisplay = 'SMS not intercepted';
+            // });
+            // function deletenewarrivant(sender) {
+            //     var filter = {
+            //         box : 'inbox',
+            //         address : sender
+            //     };
+            //     if(SMS) SMS.deleteSMS(filter, function(n){
+            //         console.log('Deleted');
+            //     }, function(err){
+            //         console.log('Not deleted');
+            //         $scope.errDisplay = 'SMS not deleted : ' + err;
+            //     });
+            // }
             if(SMS) SMS.listSMS(filter, function(data){
-                $scope.log2 = '--> SMS sent to me !';
-                $scope.log3 = '-----> ' + data;
-                $scope.log6 = '--------> ' + data[0].body;
-                $scope.log7 = '--------> ' + data[1].body;
-                $scope.log8 = '--------> ' + data[2].body;
-                $scope.log9 = '--------> ' + data[3].body;
-                $scope.log10 = '--------> ' + data[4].body;
-                $scope.log11 = '--------> ' + data[5].body;
-                $scope.log12 = '--------> ' + data[6].body;
-                $scope.log13 = '--------> ' + data[7].body;
+                console.log('############# Listing SMS...');
+                setTimeout(function(){
+                    for (var i = 0; i < data.length; i++) {
+                        switch (data[i].body) {
+                            case '1':
+                            $scope.results1 += 1;
+                            $scope.apply();
+                            break;
+                            case '2':
+                            $scope.results2 += 1;
+                            $scope.apply();
+                            break;
+                            case '3':
+                            $scope.results3 += 1;
+                            $scope.apply();
+                            break;
+                            case '4':
+                            $scope.results4 += 1;
+                            $scope.apply();
+                            break;
+                            case '5':
+                            $scope.results5 += 1;
+                            $scope.apply();
+                            break;
+                            case '6':
+                            $scope.results6 += 1;
+                            $scope.apply();
+                            break;
+                            default:
+                            $scope.results7 += 1;
+                            $scope.apply();
+                        }
+                    }
+                }, 60000);
             },
             function(err){
-                $scope.log4 = '--> SMS not sent...';
-                $scope.log5 = '----> ' + err;
+                console.log(err);
+                $scope.errDisplay = err;
             });
         });
     };
+    // $scope.stopSMSWatch = function() {
+    //     $ionicPlatform.ready(function() {
+    //         if(SMS) SMS.stopWatch(function(){
+    //             console.log('Watch ended');
+    //             $scope.watchStopped = '(Watch is over)';
+    //         }, function(){
+    //             console.log('Watch not ended');
+    //             $scope.errDisplay = 'Watch not ended ';
+    //         });
+    //     });
+    // };
 }])
